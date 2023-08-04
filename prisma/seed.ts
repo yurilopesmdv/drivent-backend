d4b4fefd6a26aaf79f347d5f3eded6b28354d5a9
yurilopesm <yurilopesm@hotmail.com>
2023-08-04 13:50:51 -0300

    feat: updating seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 import dayjs from "dayjs";
+import bcrypt from 'bcrypt';
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.event.deleteMany({});
+  await prisma.hotel.deleteMany({});
+  await prisma.room.deleteMany({});
+  await prisma.address.deleteMany({});
+  await prisma.payment.deleteMany({});
+  await prisma.ticket.deleteMany({});
+  await prisma.ticketType.deleteMany({});
+  await prisma.enrollment.deleteMany({});
+  //await prisma.user.deleteMany({});
   let event = await prisma.event.findFirst();
+  let hotel = await prisma.hotel.findFirst();
+  let room = await prisma.room.findFirst();
+  let user = await prisma.user.findFirst();
+  let enrollment = await prisma.enrollment.findFirst();
+  let address = await prisma.address.findFirst();
+  let ticketType = await prisma.ticketType.findFirst();
+  let ticket = await prisma.ticket.findFirst();
+  let payment = await prisma.payment.findFirst();
+  //let booking = await prisma.booking.findFirst();
+
   if (!event) {
     event = await prisma.event.create({
       data: {
@@ -16,7 +36,126 @@ async function main() {
     });
   }
 
-  console.log({ event });
+  if (!hotel) {
+    hotel = await prisma.hotel.create({
+      data: {
+        name: "Driven.t hotel",
+        image: "https://files.driveneducation.com.br/images/logo-rounded.png",
+        updatedAt: dayjs().toDate()
+      },
+    });
+  }
+
+  if (room) {
+    room = await prisma.room.create({
+      data: {
+        name: "Driven.t room",
+        capacity: 3,
+        hotelId: hotel.id,
+        updatedAt: dayjs().toDate()
+      },
+    });
+  }
+
+  if (!user) {
+    const hashedPassword = await bcrypt.hash("123456", 12);
+    user = await prisma.user.create({
+      data: {
+        email: "email@email.com",
+        password: hashedPassword,
+        updatedAt: dayjs().toDate()
+      },
+    });
+  }
+
+  if (!enrollment) {
+    enrollment = await prisma.enrollment.create({
+      data: {
+        name: "joao",
+        cpf: "06459861198",
+        birthday: dayjs().toDate(),
+        phone: "61 9 81617008",
+        userId: user.id
+      },
+    });
+  }
+
+  if (!address) {
+    address = await prisma.address.create({
+      data: {
+        cep: "71725053",
+        street: 'Avenida Brigadeiro Faria Lima',
+        city: 'São Paulo',
+        state:  'Itaim Bibi',
+        number: "10",
+        neighborhood:  'Itaim Bibi',
+        addressDetail:  'de 3252 ao fim - lado par',
+        enrollmentId: enrollment.id
+      },
+    });
+  }
+
+  if (!ticketType) {
+    ticketType = await prisma.ticketType.create({
+      data: {
+        name: "Online",
+        price: 100,
+        isRemote: true,
+        includesHotel: false
+      },
+    });
+    await prisma.ticketType.create({
+      data: {
+        name: 'Presencial',
+        price: 250,
+        isRemote: false,
+        includesHotel: false
+      },
+    })
+    await prisma.ticketType.create({
+      data: {
+        name: 'Presencial',
+        price: 600,
+        isRemote: false,
+        includesHotel: true
+      },
+    })
+    
+  }
+
+  if (!ticket) {
+    ticket = await prisma.ticket.create({
+      data: {
+        ticketTypeId: ticketType.id,
+        enrollmentId: enrollment.id,
+        status: "PAID"
+      },
+    });
+  }
+
+  if (!payment) {
+    payment = await prisma.payment.create({
+      data: {
+        ticketId: ticket.id,
+        value: 13,
+        cardIssuer: "Visa",
+        cardLastDigits: "2323"
+      },
+    });
+  }
+  /*
+  if (!booking) {
+    booking = await prisma.booking.create({
+      data: {
+        userId: user.id,
+        roomId: room.id,
+        updatedAt: dayjs().toDate()
+      },
+    });
+  }
+  */
+  console.log({ event, hotel, room, user, enrollment, address, ticketType, ticket, payment });
+
 }
 
 main()
